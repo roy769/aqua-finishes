b4997a1a127a75063428841ab4deb065658333b3
roy769 <roy@mortgallery.com>
2018-10-11 10:04:24 -0700

    images and form submission

---
+++ public/js/logic.js
@@ -0,0 +1,14 @@
+// IDEA: iterate urls
+//http://www.outdoorsignature.com/wp-content/uploads/2017/03/slide-1-1.jpg
+//http://www.outdoorsignature.com/wp-content/uploads/2017/03/slide-2-1.jpg
+//http://www.outdoorsignature.com/wp-content/uploads/2017/03/slide-3-1.jpg
+//http://www.outdoorsignature.com/wp-content/uploads/2017/03/slide-4-1.jpg
+//http://www.outdoorsignature.com/wp-content/uploads/2017/03/slide-5-1.jpg
+//http://www.outdoorsignature.com/wp-content/uploads/2017/03/slide-6-1.jpg
+//http://www.outdoorsignature.com/wp-content/uploads/2017/03/slide-7-1.jpg
+//http://www.outdoorsignature.com/wp-content/uploads/2017/03/slide-8-1.jpg
+//http://www.outdoorsignature.com/wp-content/uploads/2017/03/slide-9-1.jpg
+//http://www.outdoorsignature.com/wp-content/uploads/2017/03/slide-10-1.jpg
+//http://www.outdoorsignature.com/wp-content/uploads/2017/03/slide-11-1.jpg
+//http://www.outdoorsignature.com/wp-content/uploads/2017/03/slide-12-1.jpg
+//http://www.outdoorsignature.com/wp-content/uploads/2017/03/slide-13-1.jpg
